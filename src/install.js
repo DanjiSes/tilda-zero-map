79f19a2be5c661a40062f9c389de9b84156372dc
Den <danil.savchenko.dev@gmail.com>
2021-06-22 16:17:13 +0600

    Fix dev mode

--- src/install.js
+++ src/install.js
@@ -1,16 +1,23 @@
 /* eslint-disable no-var */
 
 $(function() {
-  var hash = '[hash]'
-  var devMode = window._devMode || window.location.hostname.split('.').reverse()[0] === 'ws'
+  var hash = '89a2d99916d63696efdc';
+  var devMode =
+    window._devMode !== undefined ?
+      window._devMode :
+      window.location.hostname.split('.').reverse()[0] === 'ws';
 
-  var script = document.createElement('script')
-  script.type = 'text/javascript'
-  script.src = devMode ? 'http://savchenko.sl:3000/bundle.js' : 'https://danjises.github.io/tilda-search-map-zero/dist/bundle.' + hash + '.js'
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = devMode ?
+    'http://savchenko.sl:3000/bundle.js' :
+    'https://danjises.github.io/tilda-search-map-zero/dist/bundle.' +
+      hash +
+      '.js';
 
-  var link = document.createElement('link')
+  var link = document.createElement('link');
   // link.rel = 'stylesheet'
   // link.href = devMode ? 'http://savchenko.sl:3000/bundle.css' : 'https://danjises.github.io/tilda-search-map-zero/dist/bundle.' + hash + '.css'
 
-  $('body').append([script, link])
-})
+  $('body').append([script, link]);
+});
